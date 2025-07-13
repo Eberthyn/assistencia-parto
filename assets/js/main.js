1e2860953a3c207d9b878950e8532a08ce6c7167
Eberthyn Marques <eberthyn@gmail.com>
2025-07-13 05:31:04 -0300

    Ajustes modulo 1 v3

--- assets/js/main.js
+++ assets/js/main.js
@@ -288,18 +288,31 @@ $("#myModal").on("shown.bs.modal", function () {
   $("#myInput").trigger("focus");
 });
 
-$("#example").tooltip(options);
+$(document).ready(function () {
+  function getMaiorScrollHeight() {
+    var h1 = $('#collapse50anos')[0].scrollHeight * ($('#collapse50anos').hasClass('collapsing') || $('#collapse50anos').hasClass('show'));
+    var h2 = $('#collapseXX')[0].scrollHeight * ($('#collapseXX').hasClass('collapsing') || $('#collapseXX').hasClass('show'));
+    return Math.max(
+      $('#collapse50anos').hasClass('collapsing') || $('#collapse50anos').hasClass('show') ? $('#collapse50anos')[0].scrollHeight : 0,
+      $('#collapseXX').hasClass('collapsing') || $('#collapseXX').hasClass('show') ? $('#collapseXX')[0].scrollHeight : 0
+    );
+  }
 
-$(function () {
-  $(".example-popover").popover({
-    container: "body",
-  });
+  function updateMargin(marginHeight) {
+    $('#accordionExterno').css('margin-top', marginHeight + 'px');
+  }
+
+  function ajustaMargem() {
+    var h = getMaiorScrollHeight();
+    updateMargin(h);
+  }
+
+  $('#collapse50anos, #collapseXX')
+    .on('show.bs.collapse shown.bs.collapse hide.bs.collapse hidden.bs.collapse', function () {
+      // Timeout garante que pegamos a altura certa após a transição iniciar
+      setTimeout(ajustaMargem, 10);
+    });
+
+  // Estado inicial
+  ajustaMargem();
 });
-
-$(function () {
-  $('[data-toggle="popover"]').popover();
-});
-
-$("#example").popover(options);
-
-$("#element").popover("show");
